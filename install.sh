a07cab463a771b7797cb4876ac29b9c9ca236e8c
Fabian Martinez <famartin@redhat.com>
2020-10-19 11:14:58 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
 
-curl https://raw.githubusercontent.com/famartinrh/appctl/master/appctl --output /tmp/appctl-tmp
-sudo chmod +x /tmp/appctl-tmp
-mv /tmp/appctl-tmp ~/bin/appctl
+curl https://raw.githubusercontent.com/famartinrh/appctl/master/appctl --output ~/bin/appctl
+chmod +x ~/bin/appctl
